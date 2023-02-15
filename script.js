e2178d7132b4a48d71a61b9a89a7c27c2e769bc6
Pechinkin Sergey <pechinkinsergey@yandex.ru>
2023-02-15 16:42:30 +0400

    for while do...while

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const  numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+// while(numberOfFilms === null || numberOfFilms === ''){
+//   console.log(numberOfFilms);
+//   numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+// }
+
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,13 +15,73 @@ const personalMovieDB = {
   privat: false,
 }
 
-const lastMovieOne = prompt("Один из последних просмотренных фильмов?");
-const gradeOne = prompt("На сколько оцените его?");
+let i = 0;
 
-const lastMovieTwo = prompt("Один из последних просмотренных фильмов?");
-const gradeTwo = prompt("На сколько оцените его?");
 
-personalMovieDB.movies[lastMovieOne] = gradeOne;
-personalMovieDB.movies[lastMovieTwo] = gradeTwo;
+do {
+  let movie = prompt("Один из последних просмотренных фильмов?")
+    let grade = prompt("На сколько оцените его?")
+  
+    if(movie != null && grade != null && movie != '' && grade != '' && movie.length < 5) {
+      personalMovieDB.movies[movie] = grade; 
+      console.log('done');
+    } else {
+      console.log('error');
+      i--;
+    }
+  
+    i++;
+} while ( i < 2);
+
+// while(i < 2) {
+
+//   let movie = prompt("Один из последних просмотренных фильмов?")
+//   let grade = prompt("На сколько оцените его?")
+
+//   if(movie != null && grade != null && movie != '' && grade != '' && movie.length < 5) {
+//     personalMovieDB.movies[movie] = grade; 
+//     console.log('done');
+//   } else {
+//     console.log('error');
+//     i--;
+//   }
+
+//   i++;
+// }
+
+// for(let i = 0; i < 2; i++){   /// Переписать циклом while do...while
+
+//   let movie = prompt("Один из последних просмотренных фильмов?")
+//   let grade = prompt("На сколько оцените его?")
+
+//   if(movie != null && grade != null && movie != '' && grade != '' && movie.length < 5) {
+//     personalMovieDB.movies[movie] = grade; 
+//     console.log('done');
+//   } else {
+//     console.log('error');
+//     i--;
+//   }
+
+  // let movie = prompt("Один из последних просмотренных фильмов?")
+  // while(movie === null || movie === '' || movie.length > 5){
+  //   movie = prompt("Один из последних просмотренных фильмов?");
+  // }
+  // let grade = prompt("На сколько оцените его?")
+  // while(grade === null || grade ===''){
+  //   grade = prompt("На сколько оцените его?");
+  // }
+
+  // personalMovieDB.movies[movie] = grade; 
+//}
 
 console.log(personalMovieDB);
+
+if(personalMovieDB.count < 10 && personalMovieDB.count != 0 ){
+  console.log("Просмотрено довольно мало фильмов");
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log("Вы классический зритель")
+} else if(personalMovieDB.count > 30) {
+  console.log("Вы киноман")
+} else {
+  console.log("Произошла ошибка")
+}
